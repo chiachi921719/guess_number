052aaa4a567c8af2f4537f662c13191307d0d1d3
chiachi921719 <60145995+chiachi921719@users.noreply.github.com>
2021-08-28 15:54:28 +0800

    Revert "Create guess_number.py"
    This reverts commit 64a1031ede462f0e61ae88903f3b3576e902436b.

--- guess_number.py
+++ guess_number.py
@@ -4,13 +4,17 @@
 # 猜錯的話要告述他比答案大/小
 import random
 number = random.randint(1, 100)
+time = 0
 while True:
+	time += 1 # 等於 time = time +1
 	question = input("請猜數字(1~100)：")
 	question = int(question)
 	if question == number:
-		print("你猜對了！")	
+		print("你猜對了！")
+		print("這是你猜的第", time, "次")	
 		break
 	elif question > number:
 		print("數字太大了，再試一次！")
 	elif question < number:
 		print("數字太小了，再試一次！")
+	print("這是你猜的第", time, "次")
