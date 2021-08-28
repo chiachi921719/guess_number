64a1031ede462f0e61ae88903f3b3576e902436b
chiachi921719 <60145995+chiachi921719@users.noreply.github.com>
2021-08-28 15:06:58 +0800

    Create guess_number.py

---
+++ guess_number.py
@@ -0,0 +1,16 @@
+# 產生一個隨機整數1~100（不要印出來
+# 讓使用者重複輸入數字去猜
+# 猜對的話 印出"終於猜對了！"
+# 猜錯的話要告述他比答案大/小
+import random
+number = random.randint(1, 100)
+while True:
+	question = input("請猜數字(1~100)：")
+	question = int(question)
+	if question == number:
+		print("你猜對了！")	
+		break
+	elif question > number:
+		print("數字太大了，再試一次！")
+	elif question < number:
+		print("數字太小了，再試一次！")
